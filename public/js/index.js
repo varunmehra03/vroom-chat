eb82b8c9d507c5cd5a78c6a2d28aa9421fba97d2
Varun Mehra <varunmehra03@yahoo.com>
2017-12-24 12:45:07 +0530

    Added form and minimal UI for testing

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,17 @@ socket.on('disconnect', function(){
 socket.on('newMessage', function(msg){
     console.log('newMessage');
     console.log(msg);
+    var li = jQuery('<li></li>');
+    li.text(`${msg.from}: ${msg.text}`);
+    jQuery("#messages").append(li);
 });
 
-socket.emit('createMessage',{
-    from:"Varun ",
-    text:"Hello! How are you ?"
+jQuery('#chat-form').on('submit', function(e){
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from:'User',
+        text:jQuery('input[name=message]').val()
+    }, function(serverResp){
+        console.log(serverResp);
+    });
 });
--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const socketIO = require('socket.io');
 const http = require('http');
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -13,18 +12,31 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket)=>{
     console.log("New User Connected");
+    socket.broadcast.emit('newMessage', {
+        from:'Admin',
+        text:'New User Joined',
+        createdAt: new Date().getTime()
+    });
+
+    socket.emit('newMessage', {
+        from:'Admin',
+        text:'Welcome to Vroom Chat',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('disconnect',()=>{
         console.log("User was disconnected");
     });
 
-    socket.emit('newMessage', {
-        from:"Varun",
-        text:"All Good",
-        createdAt: new Date().getTime()
-    });
+    socket.on('createMessage', (newMsg, callback) =>{
+        //io.emit to emit to all connected clients.
+        io.emit('newMessage', {
+            from:newMsg.from,
+            text:newMsg.text,
+            createdAt: new Date().getTime()
+        });
 
-    socket.on('createMessage', (newMsg) =>{
-        console.log("Create Email ", newMsg);
+        callback("Yeah!! Server Acknowledged receiving the message");
     });
 });
 
